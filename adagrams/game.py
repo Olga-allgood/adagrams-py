91b4efc07f89e7829ebb537dd11b12af35164d76
Olga-allgood <olga.s.orlova@gmail.com>
2025-09-28 16:08:36 -0700

    Updated version following instructor's feedback

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,6 +1,13 @@
 from random import randint
 
-import random
+HAND_SIZE = 10
+
+BONUS_MIN_LENGTH = 7
+
+LENGTH_BONUS_POINTS = 8
+
+PAIR_WORD = 0
+PAIR_SCORE = 1
 
 LETTER_POOL = {
     'A': 9, 
@@ -33,39 +40,48 @@ LETTER_POOL = {
 
 def draw_letters():
 
-  all_letter_occurencies = ""
+  all_letter_occurencies = []
 
   hand_of_letters = []
 
   for letter, frequency in LETTER_POOL.items():
-    all_letter_occurencies += letter *frequency
+    all_letter_occurencies.extend([letter] *frequency)
 
-  all_letter_occurencies = list(all_letter_occurencies)  
+  while len(hand_of_letters) < HAND_SIZE:
+    # random_index = randint(0, len(all_letter_occurencies)-1)
+    # random_letter = all_letter_occurencies[random_index]
+    # hand_of_letters.append(random_letter)
+    # all_letter_occurencies.pop(random_index)
 
-  while len(hand_of_letters) < 10:
-    random_index = random.randint(0, len(all_letter_occurencies)-1)
-    random_letter = all_letter_occurencies[random_index]
-    hand_of_letters.append(random_letter)
-    all_letter_occurencies.pop(random_index)
+    index = randint(0, len(all_letter_occurencies)-1)
+    all_letter_occurencies[index], all_letter_occurencies[-1] = all_letter_occurencies[-1], all_letter_occurencies[index]
+    popped = all_letter_occurencies.pop()
+    hand_of_letters.append(popped)
 
   return hand_of_letters   
 
+# helper fuction letter frequency dictionary
 
+def create_letter_frequency_dict(letters):
+  letter_frequency_dict = {}
+  for letter in letters:
+    letter_frequency_dict[letter] = letter_frequency_dict.get(letter, 0) +1
+  return letter_frequency_dict
 
 def uses_available_letters(word, letter_bank):
   word = word.upper()
   letter_bank = [letter.upper() for letter in letter_bank]
 
-  word_dict ={}
-  letter_bank_dict={}
+  word_dict = create_letter_frequency_dict(word)
+  letter_bank_dict= create_letter_frequency_dict(letter_bank)
 
-  for letter in word:
-    word_dict[letter] = word_dict.get(letter, 0)+1
-  print(word_dict)
+  # for letter in word:
+  #   word_dict[letter] = word_dict.get(letter, 0)+1
+  # # print(word_dict)
 
-  for letter in letter_bank:
-    letter_bank_dict[letter] = letter_bank_dict.get(letter, 0)+1
-  print(letter_bank_dict)
+  # for letter in letter_bank:
+  #   letter_bank_dict[letter] = letter_bank_dict.get(letter, 0)+1
+  # # print(letter_bank_dict)
 
   for letter, count in word_dict.items():
     if (letter not in letter_bank_dict 
@@ -73,29 +89,38 @@ def uses_available_letters(word, letter_bank):
       return False
   return True
 
+# score_chart = {
+#     ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"): 1,
+#     ("D", "G"): 2,
+#     ("B", "C", "M", "P"): 3,
+#     ("F", "H", "V", "W", "Y"): 4,
+#     ("K",): 5,
+#     ("J", "X"): 8,
+#     ("Q", "Z"): 10
+# }
 score_chart = {
-    ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"): 1,
-    ("D", "G"): 2,
-    ("B", "C", "M", "P"): 3,
-    ("F", "H", "V", "W", "Y"): 4,
-    ("K",): 5,
-    ("J", "X"): 8,
-    ("Q", "Z"): 10
+    'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+    'D': 2, 'G': 2,
+    'B': 3, 'C': 3, 'M': 3, 'P': 3,
+    'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+    'K': 5,
+    'J': 8, 'X': 8,
+    'Q': 10, 'Z': 10
 }
 def score_word(word):
   word = word.upper()
-  if word == "":
-    return 0
+  # if word == "":
+  #   return 0
 
   score = 0
   for letter in word:
-      for tuple_of_letters, score_of_letter in score_chart.items():
-        if letter in tuple_of_letters:
-         score += score_of_letter
+      if letter in score_chart:
+         score += score_chart.get(letter, 0)
   
-  if 7 <= len(word) <=10:
-    score += 8  
+  if len(word) >= BONUS_MIN_LENGTH:
+    score += LENGTH_BONUS_POINTS
   return score  
+
 
 def get_highest_word_score(word_list):
   word_list = [word.upper() for word in word_list]
@@ -111,23 +136,23 @@ def get_highest_word_score(word_list):
   # Find max score
   max_score = 0
   for tpl in list_of_tuples:
-    if tpl[1] > max_score:
-      max_score = tpl[1]
+    if tpl[PAIR_SCORE] > max_score:
+      max_score = tpl[PAIR_SCORE]
 
   # Collect all tuples with max score
   list_of_tuples_tie = []
   for tpl in list_of_tuples:
-    if tpl[1] == max_score:
+    if tpl[PAIR_SCORE] == max_score:
       list_of_tuples_tie.append(tpl)
 
   # Tie-breaker: prefer 10-letter words, else shortest word
   length_shortest_word = 11
   tuple_with_shortest_word = None
   for tpl in list_of_tuples_tie:
-    if len(tpl[0]) == 10:
+    if len(tpl[PAIR_WORD]) == 10:
       return tpl
-    elif len(tpl[0]) < length_shortest_word:
-      length_shortest_word = len(tpl[0])
+    elif len(tpl[PAIR_WORD]) < length_shortest_word:
+      length_shortest_word = len(tpl[PAIR_WORD])
       tuple_with_shortest_word = tpl
 
   return tuple_with_shortest_word
